7a0a8ac34ef084f937d6ec07e89f6cd5692bd0db
Milwayky <milwaycy@gmail.com>
2026-05-30 12:37:01 +0600

    feat: register Commands.Macro dependency in IoC

---
+++ SpaceBattle/SpaceBattle.Test/RegisterIoCDependencyMacroCommandTest.cs
@@ -0,0 +1,35 @@
+namespace SpaceBattle.Tests;
+
+using App;
+using Moq;
+using App.Scopes;
+
+using SpaceBattle.Lib;
+using Xunit;
+using System.Collections.Generic;
+
+public class RegisterIoCDependencyMacroCommandTest
+{
+    public RegisterIoCDependencyMacroCommandTest()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+    }
+
+    [Fact]
+    public void TestExecuteRegisterIoCDependencyMacroCommandResolvesDependency()
+    {
+        var registerCommand = new RegisterIoCDependencyMacroCommand();
+        var mockCmd1 = new Mock<SpaceBattle.Lib.ICommand>();
+        var mockCmd2 = new Mock<SpaceBattle.Lib.ICommand>();
+        var commandList = new List<SpaceBattle.Lib.ICommand> { mockCmd1.Object, mockCmd2.Object };
+
+        registerCommand.Execute();
+
+        var macroCmd = Ioc.Resolve<SpaceBattle.Lib.ICommand>("Commands.Macro", commandList);
+
+        Assert.NotNull(macroCmd);
+        Assert.IsType<MacroCommand>(macroCmd);
+    }
+}
