da80a48d0757693baa8260a89526136c63ecabd0
Milwayky <milwaycy@gmail.com>
2026-05-30 12:19:45 +0600

    feat: implement MacroCommand

---
+++ SpaceBattle/SpaceBattle.Lib/MacroCommand.cs
@@ -0,0 +1,20 @@
+namespace SpaceBattle.Lib;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class MacroCommand : ICommand
+{
+    private readonly IEnumerable<ICommand> _commands;
+
+    public MacroCommand(IEnumerable<ICommand> commands)
+    {
+        _commands = commands;
+    }
+
+    public void Execute()
+    {
+        _commands.ToList().ForEach(c => c.Execute());
+    }
+}
+
