0fd1d049d600269c80775eb9a6f689d608583d25
Milwayky <milwaycy@gmail.com>
2026-06-06 11:22:16 +0600

    fix: torpedo

--- SpaceBattle/SpaceBattle.Lib/SpaceTorpedo.cs
+++ SpaceBattle/SpaceBattle.Lib/SpaceTorpedo.cs
@@ -1,8 +1,6 @@
-using App;
-
 namespace SpaceBattle.Lib;
 
-public class SpaceTorpedo : IShootable, IMoving
+public class SpaceTorpedo : IShootable, IMoving, IWeaponized
 {
     private const string PosKey = "Position";
     private const string VelKey = "Velocity";
@@ -12,16 +10,33 @@ public class SpaceTorpedo : IShootable, IMoving
 
     public SpaceTorpedo(IDictionary<string, object> data) => _data = data;
 
-    public Vector Position { get => (Vector)_data[PosKey]; set => UpdateOrAddData(PosKey, value); }
-    public Vector Velocity { get => (Vector)_data[VelKey]; set => UpdateOrAddData(VelKey, value); }
-    public Angle Direction { get => (Angle)_data[DirKey]; set => UpdateOrAddData(DirKey, value); }
+    public Vector Position
+    {
+        get => (Vector)_data[PosKey];
+        set => _data[PosKey] = value;
+    }
+
+    public Vector Velocity
+    {
+        get => (Vector)_data[VelKey];
+        set => _data[VelKey] = value;
+    }
+
+    public Angle Direction
+    {
+        get => (Angle)_data[DirKey];
+        set => _data[DirKey] = value;
+    }
 
     public Vector GetPosition() => Position;
     public Vector GetVelocity() => Velocity;
     public Angle GetDirection() => Direction;
 
-    private void UpdateOrAddData(string key, object value)
+    public void Initialize(Vector position, Vector velocity, Angle direction)
     {
-        _data[key] = value;
+        Position = position;
+        _data[VelKey] = velocity;
+        Direction = direction;
     }
 }
+
--- SpaceBattle/SpaceBattle.Test/SpaceTorpedoTests.cs
+++ SpaceBattle/SpaceBattle.Test/SpaceTorpedoTests.cs
@@ -1,118 +1,52 @@
-using Moq;
 using SpaceBattle.Lib;
 using Xunit;
 
-public class PhotonTorpedoTests
+namespace SpaceBattle.Tests;
+
+public class SpaceTorpedoTests
 {
     [Fact]
-    public void ShouldCorrectlyStoreAndRetrieveState()
+    public void ShouldStoreAndRetrieveState()
     {
-        var storage = new Dictionary<string, object>();
-        var torpedo = new SpaceTorpedo(storage);
-        var pos = new Vector([10, 20]);
-
+        var torpedo = new SpaceTorpedo(new Dictionary<string, object>());
+        var pos = new Vector(10, 20);
         torpedo.Position = pos;
-
         Assert.Equal(pos, torpedo.Position);
     }
 
     [Fact]
     public void ShouldBeCompatibleWithMoveCommand()
     {
-        var data = new Dictionary<string, object> {
-            ["Position"] = new Vector([0, 0]),
-            ["Velocity"] = new Vector([1, 1])
-        };
-        var torpedo = new SpaceTorpedo(data);
-        var move = new MoveCommand(torpedo);
+        var torpedo = new SpaceTorpedo(new Dictionary<string, object>
+        {
+            ["Position"] = new Vector(0, 0),
+            ["Velocity"] = new Vector(1, 1)
+        });
 
-        move.Execute();
-
-        Assert.Equal(new Vector([1, 1]), torpedo.Position);
+        new MoveCommand(torpedo).Execute();
+        Assert.Equal(new Vector(1, 1), torpedo.Position);
     }
 
     [Fact]
-    public void ShouldThrowExceptionIfDataIsMissing()
+    public void ShouldThrowWhenDataMissing()
     {
-        var emptyData = new Dictionary<string, object>();
-        var torpedo = new SpaceTorpedo(emptyData);
-        var move = new MoveCommand(torpedo);
-
-        Assert.Throws<KeyNotFoundException>(() => move.Execute());
-    }
-
-    [Fact]
-    public void ShouldThrowWhenStorageAccessFails()
-    {
-        var mockStorage = new Mock<IDictionary<string, object>>();
-        mockStorage.SetupGet(s => s[It.IsAny<string>()]).Throws(new InvalidOperationException());
-        
-        var torpedo = new SpaceTorpedo(mockStorage.Object);
-
-        Assert.Throws<InvalidOperationException>(() => { var p = torpedo.Position; });
-    }
-
-    [Fact]
-    public void ShouldCorrectlyStoreAndRetrieveVelocity()
-    {
-        var storage = new Dictionary<string, object>();
-        var torpedo = new SpaceTorpedo(storage);
-        var vel = new Vector([5, 5]);
-
-        torpedo.Velocity = vel;
-
-        Assert.Equal(vel, torpedo.Velocity);
-    }
-
-    [Fact]
-    public void ShouldThrowWhenVelocityAccessFails()
-    {
-        var mockStorage = new Mock<IDictionary<string, object>>();
-        mockStorage.SetupGet(s => s[It.IsAny<string>()]).Throws(new InvalidOperationException());
-        
-        var torpedo = new SpaceTorpedo(mockStorage.Object);
-
-        Assert.Throws<InvalidOperationException>(() => { var v = torpedo.Velocity; });
-    }
-
-    [Fact]
-    public void ShouldThrowWhenSettingVelocityFails()
-    {
-        var mockStorage = new Mock<IDictionary<string, object>>();
-        
-        mockStorage.SetupSet(s => s[It.IsAny<string>()] = It.IsAny<object>())
-                .Throws(new InvalidOperationException());
-        
-        var torpedo = new SpaceTorpedo(mockStorage.Object);
-
-        Assert.Throws<InvalidOperationException>(() => torpedo.Velocity = new Vector([1, 1]));
-    }
-
-    [Fact]
-    public void ShouldCorrectlyStoreAndRetrieveDirection()
-    {
-        var storage = new Dictionary<string, object>();
-        var torpedo = new SpaceTorpedo(storage);
-        var angle = new Angle(90);
-
-        torpedo.Direction = angle;
-
-        Assert.Equal(angle, torpedo.Direction);
+        var torpedo = new SpaceTorpedo(new Dictionary<string, object>());
+        Assert.Throws<KeyNotFoundException>(() => new MoveCommand(torpedo).Execute());
     }
 
     [Fact]
     public void GettersShouldReturnCorrectValues()
     {
-        var data = new Dictionary<string, object> {
-            ["Position"] = new Vector([1, 1]),
-            ["Velocity"] = new Vector([2, 2]),
+        var torpedo = new SpaceTorpedo(new Dictionary<string, object>
+        {
+            ["Position"] = new Vector(1, 1),
+            ["Velocity"] = new Vector(2, 2),
             ["Direction"] = new Angle(45)
-        };
-        var torpedo = new SpaceTorpedo(data);
+        });
 
-        Assert.Equal(torpedo.Position, torpedo.GetPosition());
-        Assert.Equal(torpedo.Velocity, torpedo.GetVelocity());
-        Assert.Equal(torpedo.Direction, torpedo.GetDirection());
+        Assert.Equal(new Vector(1, 1), torpedo.GetPosition());
+        Assert.Equal(new Vector(2, 2), torpedo.GetVelocity());
+        Assert.Equal(new Angle(45), torpedo.GetDirection());
     }
 }
 
